3165fbf21ecdd3b60f0f5702f0242196859e83c5
vmysoulinflames <vmysoulinflames@yahoo.com>
2020-12-20 21:45:00 +0300

    ADD eye tracking for faces with libruary haarcascade_eye

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import cv2
 
 
 face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_eye.xml')
 
-img = cv2.imread('222.jpeg')
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+pic = cv2.VideoCapture(0)
 
-faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-
-cv2.imshow('result', img)
-cv2.waitKey()
+while True:
+    success, img = pic.read()
+    # img = cv2.imread('222.jpeg')
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+        img_gray_face = img_gray[y:y+h,x:x+w]
+        eyes = eye_cascade.detectMultiScale(img_gray_face, 1.1, 19)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(img, (x+ex, y+ey), (x+ex + ew, y+ey + eh), (255, 0q, 0), 2)
+    cv2.imshow('result', img)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+pic.release()
+cv2.destroyAllWindows()
